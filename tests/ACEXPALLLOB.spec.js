b6dbdf65dd48d703ec131f3d6e78b7bd8bdff078
avelmuruganofficials-sudo <a.velmuruganofficials@gmail.com>
2026-02-25 12:17:25 +0530

    first commit

--- tests/ACEXPALLLOB.spec.js
+++ tests/ACEXPALLLOB.spec.js
@@ -10,7 +10,7 @@ test('Excel data based automation', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password' }).fill('Test@123');
   await page.getByRole('button', { name: 'Login' }).click();
 
-  for (let i = 13; i < data.length; i++) {
+  for (let i = 15; i < data.length; i++) {
      const row = data[i];
     console.log(`Starting row ${i + 1}`);
     try {
